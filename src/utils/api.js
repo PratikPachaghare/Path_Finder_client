525210e7a60978eec20b70745a334e3b1c1c39b3
PratikPachghare <shampachghare@gmail.com>
2026-02-06 16:43:55 +0530

    change server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
 // export const  bass_URL = import.meta.env.BASS_URL 
-export const  bass_URL = 'http://localhost:3000/api' 
+// export const  bass_URL = 'http://localhost:3000/api' 
 // export const  bass_URL = 'https://path-finder-server.onrender.com/api' 
+export const  bass_URL = 'https://path-finder-server-1.onrender.com/api' 
